7485a9bcbfe589bb01281140a85a5ba363e56735
tahsinaltabib <nabilarah33@gmail.com>
2025-06-21 12:01:18 +0600

    logic update

--- script.js
+++ script.js
@@ -1,4 +1,62 @@
-console.log("Tahsin Al Tabib.");
+//varibale => var, let, const
+//condition
+//loop
+//array
+//function
+
+//var x=10
+//var x=20
+//console.log(x);
+
+//let x=10
+//let x=20
+//console.log(x);
+
+//const x=10
+//const x=20
+//console.log(x);
+
+
+
+let Rain=false
+
+if(Rain){
+  console.log("I won't go to university.");
+  
+}else{
+  console.log("I will go to university.")
+}
+
+
+
+let age = 20
+let selary = 30000
+let home= "Bhola"
+
+if(age>18){
+  if(selary>=30000){
+    if(home=="Dhaka"){
+      console.log("Biye krte prba!");
+      
+    }else{
+      console.log("Amdr meye k biye krte hole Dhaka bari thakte hobe! Bujjo?");
+      
+    }
+    
+  }else{
+    console.log("Aro selary lgbe. Amdr meye k biye krte hole minimum 50000 selary thakte hobe!" );
+    
+  }
+}else{
+  console.log("Aage boro hoow");
+}
+
+
+
+
+  
+  
+
 
 
 
