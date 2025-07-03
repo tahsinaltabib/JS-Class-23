84c24868fc8e5d4458947d6393536ec0f7f98cc6
tahsinaltabib <nabilarah33@gmail.com>
2025-07-03 10:20:04 +0600

    condition test

--- script.js
+++ script.js
@@ -12,19 +12,52 @@
 //let x=20
 //console.log(x);
 
-//const x=10
-//const x=20
-//console.log(x);
+// const x=10
+// const x=20
+// console.log(x);
 
 
 
+// if (condition) {
+//   //output
+// } else {
+//   //output
+// }
+  
+  
+//if it's rain, i will not go to University
+//if it's not rain, i will go to University
 
-let age = 21
-let selary = 30000
+
+// let bristi = false
+
+// if (bristi) {
+//   console.log("I'll not go to University");
+  
+// } else {
+//   console.log("I'll go to University");
+  
+// }
+
+
+// let age = 17
+
+// if (age>=18) {
+//    console.log('Biye krte prba');
+   
+// } else {
+//    console.log('Aage boro hoow');
+   
+// }
+
+
+
+let age = 19
+let selary = 35000
 let home= "Dhaka"
 
 if(age>18){
-  if(selary>=30000){
+  if(selary>=40000){
     if(home=="Dhaka"){
       console.log("Biye krte prba!");
       
@@ -34,7 +67,7 @@ if(age>18){
     }
     
   }else{
-    console.log("Aro selary lgbe. Amdr meye k biye krte hole minimum 50000 selary thakte hobe!" );
+    console.log("Aro selary lgbe. Amdr meye k biye krte hole minimum 40000 selary thakte hobe!" );
     
   }
 }else{
@@ -42,10 +75,6 @@ if(age>18){
 }
 
 
-
-
-  
-  
 
 
 
